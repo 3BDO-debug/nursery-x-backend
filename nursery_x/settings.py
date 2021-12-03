d1e73791fd38dba51a8fdcca2378b4a327eb6d43
Abdelrahman Fahmy <rooted39@gmail.com>
2021-12-03 13:27:15 +0200

    configured allowed hosts

--- nursery_x/settings.py
+++ nursery_x/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Custome auth model
 AUTH_USER_MODEL = "accounts.User"
