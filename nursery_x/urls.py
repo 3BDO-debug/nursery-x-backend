7d6ee65855e547216370a6ba67db5ddb1157c235
Abdelrahman Fahmy <rooted39@gmail.com>
2021-12-04 11:44:13 +0200

    increased the access token time limit

--- nursery_x/urls.py
+++ nursery_x/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path("announcements/", include("announcements.urls")),
     path("messenger/", include("messenger.urls")),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
